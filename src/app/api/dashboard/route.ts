e4101a81f580ba046603fa2d4338eb9404507411
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2025-04-23 15:49:58 +0200

    added files

--- src/app/api/dashboard/route.ts
+++ src/app/api/dashboard/route.ts
@@ -4,6 +4,7 @@ import { cookies } from 'next/headers';
 import jwt from 'jsonwebtoken';
 
 export async function GET() {
+    
   const cookieStore = await cookies();
   const token = cookieStore.get('authToken')?.value;
   const decoded = jwt.verify(token!, process.env.JWT_SECRET!);
